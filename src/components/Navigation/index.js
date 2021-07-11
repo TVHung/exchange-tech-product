4f3e6f97707662326b3a9c18775b5fda7fb5dfb3
truong hung <truonghungvatvo@gmail.com>
2021-07-12 00:33:08 +0700

    verify login, sign in

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,48 +1,80 @@
-import React from 'react'
-import useStyles from './styles';
-import {Link} from 'react-router-dom'
-import Grid from '@material-ui/core/Grid'
+import React, { useState, useEffect } from "react";
+import useStyles from "./styles";
+import { Link } from "react-router-dom";
+import Grid from "@material-ui/core/Grid";
+import { Button } from "@material-ui/core";
+import { toast } from "react-toastify";
 
-export default function Navigation() {
+export default function Navigation({ isAuthenticated, setAuth }) {
     const classes = useStyles();
+
+    useEffect(() => {
+        console.log("isAuthenticated", isAuthenticated);
+    });
+
+    const logout = (e) => {
+        e.preventDefault();
+        localStorage.removeItem("token");
+        setAuth(false);
+        toast.success("Logout is successfully!");
+    };
+
     return (
         <nav className={classes.container}>
             <Grid container className={classes.gridContainer}>
                 <Grid item xs={2}>
-                    <Link to='/' className={classes.linkStyle}>
+                    <Link to="/" className={classes.linkStyle}>
                         <h3>Logo</h3>
                     </Link>
                 </Grid>
                 <Grid item xs={10}>
                     <ul className={classes.ul}>
                         <li className={classes.li}>
-                            <Link to='/' className={classes.linkStyle}>
+                            <Link to="/" className={classes.linkStyle}>
                                 Home
                             </Link>
                         </li>
                         <li className={classes.li}>
-                            <Link to='/service' className={classes.linkStyle}>
+                            <Link to="/service" className={classes.linkStyle}>
                                 Service
                             </Link>
                         </li>
                         <li className={classes.li}>
-                            <Link to='/about' className={classes.linkStyle}>
+                            <Link to="/about" className={classes.linkStyle}>
                                 About
                             </Link>
                         </li>
+                        {!isAuthenticated ? (
+                            <li className={classes.li}>
+                                <Link to="/login" className={classes.linkStyle}>
+                                    Login
+                                </Link>
+                            </li>
+                        ) : null}
+                        {!isAuthenticated ? (
+                            <li className={classes.li}>
+                                <Link
+                                    to="/register"
+                                    className={classes.linkStyle}
+                                >
+                                    Sign in
+                                </Link>
+                            </li>
+                        ) : null}
+
                         <li className={classes.li}>
-                            <Link to='/login' className={classes.linkStyle}>
-                                Login
-                            </Link>
-                        </li>
-                        <li className={classes.li}>
-                            <Link to='/register' className={classes.linkStyle}>
-                                Sign in
-                            </Link>
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                className={classes.submit}
+                                onClick={(e) => logout(e)}
+                            >
+                                Logout
+                            </Button>
                         </li>
                     </ul>
                 </Grid>
             </Grid>
         </nav>
-    )
+    );
 }
---
+++ src/screens/Dashboard/index.js
@@ -0,0 +1,14 @@
+import { Button } from "@material-ui/core";
+import React, { Fragment } from "react";
+import { toast } from "react-toastify";
+import useStyles from "./styles";
+
+export default function Dashboard({ setAuth }) {
+    const classes = useStyles();
+
+    return (
+        <Fragment>
+            <div>Dashboard page</div>
+        </Fragment>
+    );
+}
