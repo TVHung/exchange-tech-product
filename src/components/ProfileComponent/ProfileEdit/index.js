0e5a9c088e5fe3efca5d7cf7ec629a5231422ba1
tvhung <truonghungvatvo@gmail.com>
2021-11-05 22:45:59 +0700

    account profile

--- src/components/ProfileComponent/ProfileEdit/index.js
+++ src/components/ProfileComponent/ProfileEdit/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./profileEdit.scss";
 
 export default function ProfileEdit() {
-  return <div className="profile-edit-container">ProfileEdit</div>;
+  return <div className="profile-edit-container"></div>;
 }
